01e11157fc9881767e024bfe130c6fadd4f3f710
Letícia Maia Marques <leticia.maia@escolar.ifrn.edu.br>
2022-12-11 17:10:23 -0300

    Login- incompleto

---
+++ code/aula/Areas/Seguranca/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using malfatti.Infraestrutura;
+using System.Web.Mvc;
+using System.Web;
+
+namespace malfatti.Areas.Seguranca.Controllers
+{
+    public class AdminController : Controller
+    {
+        // Definição da Propriedade GerenciadorUsuario
+        private GerenciadorUsuario GerenciadorUsuario
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().
+                GetUserManager<GerenciadorUsuario>();
+
+            }
+        }
+        // GET: Seguranca/Admin
+        public ActionResult Index()
+        {
+            return View(GerenciadorUsuario.Users);
+        }
+
+        public ActionResult Create()
+        { return View(); }
+    }
+}
---
+++ code/aula/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using malfatti.Areas.Seguranca.Models;
+
+namespace malfatti.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDb")
+        { }
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(
+            new IdentityDbInit());
+        }
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+
+        public System.Data.Entity.DbSet<malfatti.Areas.Seguranca.Models.Usuario> Usuarios { get; set; }
+
+        public System.Data.Entity.DbSet<malfatti.Areas.Seguranca.Models.UsuarioViewModel> UsuarioViewModels { get; set; }
+    }
+    public class IdentityDbInit :
+        DropCreateDatabaseIfModelChanges
+        <IdentityDbContextAplicacao>
+
+    {
+    }
+}
