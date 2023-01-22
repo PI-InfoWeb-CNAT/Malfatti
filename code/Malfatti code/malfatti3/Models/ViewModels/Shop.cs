568994ec624cfc6198132bbbc30ae13baf3638db
Maria-Ricarte <97289630+Maria-Ricarte@users.noreply.github.com>
2023-01-22 08:42:30 -0300

    Create Shop.cs

---
+++ code/Malfatti code/malfatti3/Models/ViewModels/Shop.cs
@@ -0,0 +1,13 @@
+using Modelo.Cadastro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace malfatti.Models.ViewModels
+{
+    public class Shop
+    {
+        public IQueryable<Produto> listaprodutos;
+    }
+}
