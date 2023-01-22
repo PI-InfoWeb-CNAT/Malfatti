24174ec908003ca599806b37daed7b33a283a1ac
Maria-Ricarte <97289630+Maria-Ricarte@users.noreply.github.com>
2023-01-22 08:41:03 -0300

    Update OrderVM.cs

--- code/Malfatti code/malfatti3/Models/ViewModels/OrderVM.cs
+++ code/Malfatti code/malfatti3/Models/ViewModels/OrderVM.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Modelo.Cart;
+using Models.Cart;
 namespace malfatti.Models.ViewModels
 {
     public class OrderVM
